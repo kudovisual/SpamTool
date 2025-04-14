f56c41aca1692fad317bb3a56cc5ca69602a9486
kudodz <154000395+kudovisual@users.noreply.github.com>
2025-04-14 21:23:41 +0700

    Update tool.py

--- tool.py
+++ tool.py
@@ -1,22 +1,21 @@
-# Code by kudoo
+# telegram: @kudo1004
 # Link: https://github.com/kudovisual/SpamTool
 import pyautogui, pyperclip, random, time
-print("Tool Spam 1.0 by kudoo")
+print("Tool Spam 2.0 by kudodz")
 print("Link: https://github.com/kudovisual/SpamTool")
+print("BẢN 2.0: FIX LỖI BỊ SAI CHỖ")
 msg = input("Nhập nội dung cần spam: ").split(" || ")
-n = int(input("Nhập số lần spam: "))
-m = float(input("Nhập thời gian delay: "))
+spam_quantity = int(input("Nhập số lần spam: "))
+delay = float(input("Nhập thời gian delay: "))
 
 print("Chuẩn bị")
-# Đếm ngược 5 giây:
 for i in range(5,0, -1):
     print(i,end="...",flush='True')
     time.sleep(1)
-print("Bắt đầu")
+print("Bắt đầu!")
 
-# Spam:
-for i in range(n):
+for i in range(spam_quantity):
     pyperclip.copy(random.choice(msg))
     pyautogui.hotkey("ctrl", "v")
     pyautogui.press("enter")
-    time.sleep(m) # Thời gian delay
+    time.sleep(delay)
